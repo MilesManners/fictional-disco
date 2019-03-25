0f4856b3bd6a5ee8be1d0a6286bb379e4253555c
Henry Kajitani <doctor.miles.manners@gmail.com>
2019-03-25 13:08:34 -0500

    Modify window settings

--- src/background.js
+++ src/background.js
@@ -15,12 +15,12 @@ let win
 protocol.registerStandardSchemes(['app'], { secure: true })
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 1500, height: 1000 })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    if (!process.env.IS_TEST) win.webContents.openDevTools({ mode: 'bottom' })
   } else {
     createProtocol('app')
     // Load the index.html when not in development
